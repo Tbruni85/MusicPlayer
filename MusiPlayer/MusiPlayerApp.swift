3567e4ac1f77a5c6a312e33a45c9fa3ac983c95c
Tiziano Bruni <tiziano.bruni@gmail.com>
2024-05-24 12:19:43 +0100

    First commit

---
+++ MusiPlayer/MusiPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiPlayerApp.swift
+//  MusiPlayer
+//
+//  Created by Tiziano Bruni on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusiPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
